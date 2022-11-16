95f80a54f4648a4f750de0371a45c90a6635f9c7
Zstone19 <stone28@illinois.edu>
2022-11-16 17:08:54 -0600

    Updated bash script

--- build_dep.sh
+++ build_dep.sh
@@ -20,47 +20,61 @@ do
     esac
 done
 
-# First, install pyCCF:
-rm -r pyCCF
-mkdir pyCCF
-cd pyCCF
-wget https://bitbucket.org/cgrier/python_ccf_code/downloads/ccf_code_package_V2.zip
-unzip ccf_code_package_V2.zip
-rm ccf_code_package_V2.zip
-cd ..
 
+function install_pyccf {
+    rm -r pyCCF
+    mkdir pyCCF
+    cd pyCCF
+    wget https://bitbucket.org/cgrier/python_ccf_code/downloads/ccf_code_package_V2.zip
+    unzip ccf_code_package_V2.zip
+    rm ccf_code_package_V2.zip
+    cd .. 
+}
 
-if [ $pval = true ]; then
-    #Next, install PLIKE
-    #NOTE: This assumes a gfortran compiler is installed
+function install_plike {
     mkdir plike_v4
     cd plike_v4
     wget http://www.weizmann.ac.il/home/tal/zdcf/plike_v4.0.f90
     gfortran plike_v4.0.f90 -o plike
-    cd ..
-fi
+    cd .. 
+}
 
+
+function install_javelin {
+    wget https://github.com/nye17/javelin/archive/refs/heads/master.zip
+    unzip master.zip
+    rm master.zip
+    mv javelin-master javelin
+    rm -r javelin-master
+
+    # Compile JAVELIN
+    cd javelin
+
+    if [[ $user = true && $fval = false ]]; then
+        python setup.py install --user
+    elif [[ $user = true && $fval != false ]]; then
+        python setup.py config_fc --fcompiler=$fval install --user
+    elif [ $fval != false ]; then
+        python setup.py config_fc --fcompiler=$fval install
+    else
+        python setup.py install
+    fi
+
+    cd .. 
+
+}
+
+
+# First, install pyCCF:
+install_pyccf || echo "Error installing pyCCF"
+
+if [ $pval = true ]; then
+    #Next, install PLIKE
+    #NOTE: This assumes a gfortran compiler is installed
+
+    install_plike || echo "Error installing PLIKE"
+fi 
 
 # Next, download JAVELIN
 # NOTE: This requires a Fortran compiler >F90 and Git, all other packages are installed with PETL
-wget https://github.com/nye17/javelin/archive/refs/heads/master.zip
-unzip master.zip
-rm master.zip
-mv javelin-master javelin
-rm -r javelin-master
-
-# Compile JAVELIN
-cd javelin
-
-
-if [[ $user = true && $fval = false ]]; then
-    python setup.py install --user
-elif [[ $user = true && $fval != false ]]; then
-    python setup.py config_fc --fcompiler=$fval install --user
-elif [ $fval != false ]; then
-    python setup.py config_fc --fcompiler=$fval install
-else
-    python setup.py install
-fi
-
-cd ..
+install_javelin || echo "Error installing JAVELIN"
