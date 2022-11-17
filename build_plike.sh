478f17f5504a4da9d563847186f844dd28ac2d4e
Zstone19 <stone28@illinois.edu>
2022-11-17 16:35:41 -0600

    Fixing setup

---
+++ build_plike.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Building PLIKE
+
+install_plike () {
+    mkdir plike_v4
+    cd plike_v4
+    wget http://www.weizmann.ac.il/home/tal/zdcf/plike_v4.0.f90 
+    gfortran plike_v4.0.f90 -o plike
+    cd .. 
+}
+
+install_plike || echo "Error installing PLIKE"
