7f627fba50eb9428bdfb9360bfeafc219fa096d6
Zstone19 <stone28@illinois.edu>
2022-11-29 21:49:22 -0600

    Fix output for drw_rej

--- src/pypetal/modules.py
+++ src/pypetal/modules.py
@@ -133,7 +133,7 @@ use_for_javelin: {}
     #Write LC fit
     dat_fname = output_dir + line_names[0] + '/drw_rej/' + line_names[0] + '_drw_fit.dat'
     header = '#time,light curve,error'
-    write_data( [ res['fit_x'], res['fit_y'], res['fit_err'] ], dat_fname, header )    
+    write_data( [ res['fit_x'].value, res['fit_y'].value, res['fit_err'].value ], dat_fname, header )    
     
     #Write the MCMC chains
     dat_fname = output_dir + line_names[0] + '/drw_rej/' + line_names[0] + '_chain.dat'
@@ -183,7 +183,7 @@ use_for_javelin: {}
         #Write LC fit
         dat_fname = output_dir + line_names[i+1] + '/drw_rej/' + line_names[i+1] + '_drw_fit.dat'
         header = '#time,light curve,error'
-        write_data( [ res['fit_x'], res['fit_y'], res['fit_err'] ], dat_fname, header )        
+        write_data( [ res['fit_x'].value, res['fit_y'].value, res['fit_err'].value ], dat_fname, header )        
         
         #Write the MCMC chains
         dat_fname = output_dir + line_names[i+1] + '/drw_rej/' + line_names[i+1] + '_chain.dat'
@@ -360,7 +360,6 @@ def pyzdcf_tot(cont_fname, line_fnames, line_names, output_dir,
     #Read general kwargs
 
     verbose = general_kwargs['verbose']
-    use_weights = general_kwargs['use_weights']
     time_unit = general_kwargs['time_unit']
     lc_unit = general_kwargs['lc_unit']
         
