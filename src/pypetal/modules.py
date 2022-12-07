08b7710757eb9e3ea76e8c79c8ab1b09948f934e
Zstone19 <stone28@illinois.edu>
2022-12-07 02:19:08 -0600

    pyCCF can now be run in parallel

--- src/pypetal/modules.py
+++ src/pypetal/modules.py
@@ -5,6 +5,8 @@ import pypetal.defaults as defaults
 
 import os
 import shutil
+import multiprocessing as mp
+from functools import partial
 
 import numpy as np
 from astropy.table import Table
@@ -262,6 +264,7 @@ def pyccf_tot(cont_fname, line_fnames, line_names, output_dir,
     time_unit = general_kwargs['time_unit']
     lc_unit = general_kwargs['lc_unit']
     lag_bounds = general_kwargs['lag_bounds']
+    threads = general_kwargs['threads']
     
     #--------------------------------------------------
     #Read kwargs
@@ -297,13 +300,18 @@ nbin: {}
         print(txt_str)
         
     
-    res_tot = []
+    pyccf_func = partial( utils.get_pyccf_lags, interp=interp, nsim=nsim, mcmode=mcmode,
+                          sigmode=sigmode, thres=thres)
+    
+    args = [ [x for x in line_fnames], [x for x in lag_bounds] ]
+    
+    pool = mp.Pool(threads)
+    res_tot = pool.starmap( pyccf_func, args )
+    
     for i in range(len(line_fnames)):
-        res = utils.get_pyccf_lags( cont_fname, line_fnames[i], 
-                                   lag_bounds=lag_bounds[i], 
-                                   interp=interp, nsim=nsim, mcmode=mcmode, 
-                                   sigmode=sigmode, thres=thres)
-
+        
+        res_tot[i]['name'] = line_names[i]
+        res = res_tot[i]
 
         #Write CCF to file
         dat_fname = output_dir + line_names[i+1] + r'/pyccf/' + line_names[i+1] + '_ccf.dat'
@@ -315,8 +323,6 @@ nbin: {}
         header = '#CCCD,CCPD'
         write_data( [ res['CCCD_lags'], res['CCPD_lags'] ], dat_fname, header )        
         
-        
-        res['name'] = line_names[i]
     
         x1, y1, yerr1 = np.loadtxt( cont_fname, delimiter=',', unpack=True, usecols=[0,1,2] )
         x2, y2, yerr2 = np.loadtxt( line_fnames[i], delimiter=',', unpack=True, usecols=[0,1,2] )
@@ -328,9 +334,6 @@ nbin: {}
                                     lc_names=[line_names[0], line_names[i+1]],
                                     fname = output_dir + line_names[i+1] + r'/pyccf/' + line_names[i+1] + '_ccf.pdf', 
                                     show=plot)
-
-    
-        res_tot.append(res)
     
     return res_tot 
 
