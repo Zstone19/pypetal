3e00e2307f68b1657356473a6755c3a87563636a
Zstone19 <stone28@illinois.edu>
2023-01-27 20:24:19 -0600

    Added tests for drw rejection, pyccf, pyzdcf, javelin, and javelin with together=True

--- tests/drw_rej_test.py
+++ tests/drw_rej_test.py
@@ -1,95 +1,181 @@
 import pypetal.pipeline as pl
 import glob
+import numpy as np 
 import shutil
 
+import unittest
 
 
-main_dir = '../examples/dat/javelin_'
-filenames = [main_dir + 'continuum.dat', main_dir + 'yelm.dat', main_dir + 'zing.dat']
+def str2bool(string_, default='raise'):
 
-output_dir = '.tmp/'
-line_names = ['continuum', 'yelm', 'zing']
-
-
-params = {
-    'reject_data': [True, False, True]
-}
-
-res = pl.run_pipeline(filenames, output_dir, line_names,
-                      run_drw_rej=True,
-                      drw_rej_params=params)
-
-
-main_directories = glob.glob('.tmp/*/')
-subdirs = ['.tmp/continuum/', '.tmp/yelm/', '.tmp/zing/']
-
-
-#Make sure each line has a subdirectory
-if '.tmp/continuum/' not in main_directories:
-    raise Exception('continuum directory not found')
-
-if '.tmp/yelm/' not in main_directories:
-    raise Exception('yelm directory not found')
-
-if '.tmp/zing/' not in main_directories:
-    raise Exception('zing directory not found')
+    true = ['true', 't', '1', 'y', 'yes', 'enabled', 'enable', 'on']
+    false = ['false', 'f', '0', 'n', 'no', 'disabled', 'disable', 'off']
+    if string_.lower() in true:
+        return True
+    elif string_.lower() in false or (not default):
+        return False
+    else:
+        raise ValueError('The value \'{}\' cannot be mapped to boolean.'
+                         .format(string_))
 
 
 
+class TestDrwRej(unittest.TestCase):
 
-#Make sure each line has a "drw_rej" subdirectory
-for fdir in sub_dirs:
-    subdirs = glob.glob(fdir + '*')
-    if fdir + 'drw_rej/' not in subdirs:
-        raise Exception('drw_rej directory not found in ' + fdir)
+    def setUp(self):
+
+        main_dir = 'test_repo/pypetal/examples/dat/javelin_'
+        filenames = [main_dir + 'continuum.dat', main_dir + 'yelm.dat', main_dir + 'zing.dat']
+
+        output_dir = '.tmp/'
+        line_names = ['continuum', 'yelm', 'zing']
+
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
 
 
-    
+        params = {
+            'nchain': 100,
+            'nburn': 50,
+            'nwalker': 20,
+            'reject_data': [True, False, True],
+            'nsig': 1.0
+        }
 
-#Make sure each "drw_rej" subdirectory has a chain,drw_fit, and mask file
-for i, fdir in enumerate(sub_dirs):
-    rej_dir = fdir + 'drw_rej/'
-    files = glob.glob(rej_dir + '*')
-    
-    if rej_dir + line_names[i] + '_chain.dat' not in files:
-        raise Exception('chain file not found in ' + rej_dir)
-    
-    if rej_dir + line_names[i] + '_drw_fit.dat' not in files:
-        raise Exception('drw_fit file not found in ' + rej_dir)
-    
-    if rej_dir + line_names[i] + '_mask.dat' not in files:
-        raise Exception('mask file not found in ' + rej_dir)
-    
-    if rej_dir + line_names[i] + '_drw_fit.pdf' not in files:
-        raise Exception('figure not found in ' + rej_dir)
-    
-    
-    
-    
-#Make sure light curves get saved
-if '.tmp/light_curves/' not in main_directories:
-    raise Exception('light curve directory not found')
 
-lc_files = glob.glob('.tmp/light_curves/*')
-if '.tmp/light_curves/continuum.dat' not in lc_files:
-    raise Exception('continuum light curve not found')
+        #Run pypetal
+        res = pl.run_pipeline(output_dir, filenames, line_names,
+                            run_drw_rej=True,
+                            drw_rej_params=params, 
+                            file_fmt='ascii')
 
-if '.tmp/light_curves/yelm.dat' not in lc_files:
-    raise Exception('yelm light curve not found')
-
-if '.tmp/light_curves/zing.dat' not in lc_files:
-    raise Exception('zing light curve not found')
+        self.filenames = filenames
+        self.line_names = line_names
+        self.res = res
 
 
 
+    #Make sure the lengths and keys of each of the resulting arrays are correct
+    def test_res(self):
+        
+        expected_keys = ['masks', 'reject_data', 'taus', 'sigmas', 'jitters']
+        expected_lengths = np.full( len(expected_keys), 3 )
 
-#Make sure "reject_data" feature works
-if '.tmp/continuum_data.dat' not in glob.glob('.tmp/*'):
-    raise Exception('continuum_data.dat not found')
+        lc_lengths = []
+        for i in range(len(self.filenames)):
+            x, _, _ = np.loadtxt(self.filenames[i], unpack=True, usecols=[0,1,2])
+            lc_lengths.append(len(x))
 
-if '.tmp/yelm_data.dat' not in glob.glob('.tmp/*'):
-    raise Exception('yelm_data.dat not found')
+        mc_length = 100*20
+
+        #Make sure the keys of the output are correct
+        res_keys = list(self.res['drw_rej_res'].keys())
+        for key in expected_keys:
+            self.assertIn( key, res_keys )
+
+
+        #Make sure lengths of key values are correct
+        for key in expected_keys:
+            self.assertEqual( len(self.res['drw_rej_res'][key]), expected_lengths[i] )
+
+
+        #Make sure yelm mask is all false
+        self.assertEqual( np.all(self.res['drw_rej_res']['masks'][1]), False )
+
+        #Make sure yelm tau, sig, and jitter are None
+        for key in['taus', 'sigmas', 'jitters']:
+            self.assertIs( self.res['drw_rej_res'][key][1], None )
+
+        #Make sure lengths of masks are correct
+        for i in range(3):
+            self.assertEqual( len(self.res['drw_rej_res']['masks'][i]), lc_lengths[i] )
+
+        #Make sure MCMC lengths are correct
+        for key in ['taus', 'sigmas', 'jitters']:
+            for i in [0,2]:
+                self.assertEqual( len(self.res['drw_rej_res'][key][i]), mc_length )
 
 
 
-shutil.rmtree('.tmp')
+
+    #Make sure the layout of the files is correct
+    def test_file_layout(self):
+        main_directories = glob.glob('.tmp/*/')
+        subdirs = ['.tmp/continuum/', '.tmp/zing/']
+
+        #Make sure each line has a subdirectory
+        self.assertIn( '.tmp/continuum/' , main_directories )
+        self.assertIn( '.tmp/yelm/' , main_directories )
+        self.assertIn( '.tmp/zing/' , main_directories )
+
+        #Make sure yelm has no files
+        self.assertEqual( len(glob.glob('.tmp/yelm/*')), 0 )
+
+        #Make sure each line has a "drw_rej" subdirectory
+        for fdir in subdirs:
+            subdir_dirs = glob.glob(fdir + '*')
+            self.assertIn( fdir + 'drw_rej' , subdir_dirs )
+
+        #Make sure each "drw_rej" subdirectory has a chain,drw_fit, and mask file
+        for i, fdir in zip([0,2], subdirs):
+            files = glob.glob(fdir + 'drw_rej/*')
+            self.assertIn( fdir + 'drw_rej/' + self.line_names[i] + '_chain.dat' , files )
+            self.assertIn( fdir + 'drw_rej/' + self.line_names[i] + '_drw_fit.dat' , files )
+            self.assertIn( fdir + 'drw_rej/' + self.line_names[i] + '_mask.dat' , files )
+            self.assertIn( fdir + 'drw_rej/' + self.line_names[i] + '_drw_fit.pdf', files )
+
+            #Make sure the mask file has the correct number of lines
+            x, _, _ = np.loadtxt(self.filenames[i], unpack=True, usecols=[0,1,2])
+
+            mask = np.loadtxt(fdir + 'drw_rej/' + self.line_names[i] + '_mask.dat', unpack=True, dtype=str)
+            mask = list(map(str2bool, mask))
+
+            self.assertEqual( len(mask), len(x) )
+
+
+
+            #Make sure the chain file has the correct number of lines
+            taus, sigs, jits = np.loadtxt(fdir + 'drw_rej/' + self.line_names[i] + '_chain.dat', unpack=True, delimiter=',', usecols=[0,1,2])            
+            self.assertEqual( len(taus), 100*20 )
+
+
+        #Make sure the light curves get saved
+        self.assertIn( '.tmp/light_curves/', main_directories )
+
+        lc_files = glob.glob('.tmp/light_curves/*')
+        for name in self.line_names:
+            self.assertIn( '.tmp/light_curves/' + name + '.dat', lc_files )
+
+        #Make sure the masked light curves get saved
+        self.assertIn( '.tmp/processed_lcs/', main_directories )
+
+        lc_files = glob.glob('.tmp/processed_lcs/*')
+        for i, name in enumerate(self.line_names):
+            self.assertIn( '.tmp/processed_lcs/' + name + '_data.dat', lc_files )
+
+            #Make sure the processed lcs have reasonable number of values
+            x, _, _ = np.loadtxt('.tmp/processed_lcs/' + name + '_data.dat', unpack=True, delimiter=',', usecols=[0,1,2])
+            xdata, _, _ = np.loadtxt(self.filenames[i], unpack=True, usecols=[0,1,2])
+            self.assertLessEqual( len(x), len(xdata) )
+
+
+
+    def test_res_file_match(self):
+
+        #Match chains
+        for i, name in zip([0,2], ['continuum', 'zing']):
+            file_tau, file_sig, file_jit = np.loadtxt('.tmp/' + name + '/drw_rej/' + name + '_chain.dat', unpack=True, delimiter=',', usecols=[0,1,2])
+            res_sig, res_tau, res_jit = self.res['drw_rej_res']['taus'][i], self.res['drw_rej_res']['sigmas'][i], self.res['drw_rej_res']['jitters'][i]
+
+            self.assertListEqual(list(file_tau), list(res_tau))
+            self.assertListEqual(list(file_sig), list(res_sig))
+            self.assertListEqual(list(file_jit), list(res_jit))
+
+
+        #Match masks
+        for i, name in zip([0,2], ['continuum', 'zing']):
+            file_mask = np.loadtxt('.tmp/' + name + '/drw_rej/' + name + '_mask.dat', unpack=True, dtype=str)
+            file_mask = list(map(str2bool, file_mask))
+
+            res_mask = self.res['drw_rej_res']['masks'][i]
+            self.assertListEqual( list(file_mask), list(res_mask) )
---
+++ tests/javelin_test.py
@@ -0,0 +1,281 @@
+import pypetal.pipeline as pl
+import numpy as np 
+
+import javelin.lcmodel
+import javelin.zylc
+
+import glob
+import os
+import shutil
+
+import unittest
+
+class TestJAVELIN(unittest.TestCase):
+
+    def setUp(self):
+
+        main_dir = 'test_repo/pypetal/examples/dat/javelin_'
+        filenames = [main_dir + 'continuum.dat', main_dir + 'yelm.dat', main_dir + 'zing.dat']
+
+        output_dir = '.tmp/'
+        line_names = ['continuum', 'yelm', 'zing']
+
+
+        if os.path.exists(output_dir) and os.path.isdir(output_dir):
+            shutil.rmtree(output_dir)
+
+
+        params = {
+            'nchain': 20,
+            'nburn': 10,
+            'nwalker': 10
+        }
+        lag_bounds = [ [-1000, 1000], [-500, 500] ]
+
+        #Run pypetal
+        res = pl.run_pipeline(output_dir, filenames, line_names,
+                            run_javelin=True,
+                            javelin_params=params, 
+                            lag_bounds=lag_bounds,
+                            file_fmt='ascii')
+
+        self.filenames = filenames
+        self.line_names = line_names
+        self.res = res
+
+
+
+
+    #Make sure the lengths and keys of each of the resulting arrays are correct
+    def test_res(self):
+        
+        expected_keys = ['cont_hpd', 'tau', 'sigma', 'tophat_params', 'hpd',
+                         'cont_model', 'rmap_model', 'cont_dat', 'tot_dat', 'bestfit_model']
+
+        num_dicts = len(self.line_names) - 1
+        self.assertEqual( num_dicts, len(self.res['javelin_res']) )
+
+        mc_length = 20*10
+
+        #Make sure the keys of the output are correct
+        for i in range(num_dicts):
+            res_keys = list(self.res['javelin_res'][i].keys())
+            for key in expected_keys:
+                self.assertIn( key, res_keys )
+
+
+        #Make sure data types are correct
+        for i in range(num_dicts):
+    
+            for j, key in enumerate(expected_keys):
+
+                if key == 'cont_model':
+                    self.assertIs( type(self.res['javelin_res'][i][key]), javelin.lcmodel.Cont_Model )
+                elif key == 'rmap_model':
+                    self.assertIs( type(self.res['javelin_res'][i][key]), javelin.lcmodel.Rmap_Model )
+                elif (key == 'cont_dat') or (key == 'tot_dat') or (key == 'bestfit_model'):
+                    self.assertIs( type(self.res['javelin_res'][i][key]), javelin.zylc.LightCurve )
+                else:
+                    self.assertIs( self.res['javelin_res'][i][key].dtype.type, np.float64 )
+
+        
+        #Make sure lengths of arrays are correct
+        for i in range(num_dicts):
+            self.assertEqual( len(self.res['javelin_res'][i]['tau']), mc_length )
+            self.assertEqual( len(self.res['javelin_res'][i]['sigma']), mc_length )
+
+            self.assertEqual( len(self.res['javelin_res'][i]['tophat_params']), 3 )
+            self.assertEqual( len(self.res['javelin_res'][i]['tophat_params'][0]), mc_length )
+            self.assertEqual( len(self.res['javelin_res'][i]['tophat_params'][1]), mc_length )
+            self.assertEqual( len(self.res['javelin_res'][i]['tophat_params'][2]), mc_length )
+
+            self.assertEqual( len(self.res['javelin_res'][i]['cont_hpd']), 3 )
+            self.assertEqual( len(self.res['javelin_res'][i]['cont_hpd'][0]), 2 )
+
+            self.assertEqual( len(self.res['javelin_res'][i]['hpd']), 3 )
+            self.assertEqual( len(self.res['javelin_res'][i]['hpd'][0]), 5 ) 
+
+
+        #Make sure that the LC objetcs have the right light curves
+        x_cont, y_cont, yerr_cont = np.loadtxt( self.filenames[0], unpack=True, usecols=[0,1,2] )
+        for i in range(num_dicts):
+            x, y, yerr = np.loadtxt(self.filenames[i+1], unpack=True, usecols=[0,1,2])
+
+            for key in ['cont', 'tot']:
+
+                #Continuum
+                jav_x = self.res['javelin_res'][i][ key + '_dat'].jlist[0]
+                jav_y = self.res['javelin_res'][i][ key + '_dat'].mlist[0] + self.res['javelin_res'][i][ key + '_dat'].blist[0]
+                jav_yerr = self.res['javelin_res'][i][ key + '_dat'].elist[0]
+
+                self.assertListEqual( list(x_cont), list(jav_x) )
+                self.assertListEqual( list(yerr_cont), list(jav_yerr) )
+
+                for j in range(len(y_cont)):
+                    self.assertAlmostEqual( y_cont[j], jav_y[j], places=6 )
+
+
+                #Line
+                if key == 'rmap':
+                    jav_x = self.res['javelin_res'][i][ key + '_dat'].jlist[1]
+                    jav_y = self.res['javelin_res'][i][ key + '_dat'].blist[1] + + self.res['javelin_res'][i][ key + '_dat'].blist[1]
+                    jav_yerr = self.res['javelin_res'][i][ key + '_dat'].elist[1]
+
+                    self.assertListEqual( list(x), list(jav_x) )
+                    self.assertListEqual( list(yerr), list(jav_yerr) )
+
+                    for j in range(len(y)):
+                        self.assertAlmostEqual( y[j], jav_y[j], places=6 )
+
+
+
+
+        #Make sure that the LCModel objects have the right light curves
+        for i in range(num_dicts):
+            x, y, yerr = np.loadtxt(self.filenames[i+1], unpack=True, usecols=[0,1,2])
+
+            for key in ['cont', 'rmap']:
+
+                #Continuum
+                jav_x = self.res['javelin_res'][i][ key + '_model'].zydata.jlist[0]
+                jav_y = self.res['javelin_res'][i][ key + '_model'].zydata.mlist[0] + self.res['javelin_res'][i][ key + '_model'].zydata.blist[0]
+                jav_yerr = self.res['javelin_res'][i][ key + '_model'].zydata.elist[0]
+
+                self.assertListEqual( list(x_cont), list(jav_x) )
+                self.assertListEqual( list(yerr_cont), list(jav_yerr) )
+                for j in range(len(y_cont)):
+                    self.assertAlmostEqual( y_cont[j], jav_y[j], places=6 )
+
+
+                #Line
+                if key == 'rmap':
+                    jav_x = self.res['javelin_res'][i][ key + '_model'].zydata.jlist[1]
+                    jav_y = self.res['javelin_res'][i][ key + '_model'].zydata.mlist[1] + self.res['javelin_res'][i][ key + '_model'].zydata.blist[1]
+                    jav_yerr = self.res['javelin_res'][i][ key + '_model'].zydata.elist[1]
+
+                    self.assertListEqual( list(x), list(jav_x) )
+                    self.assertListEqual( list(yerr), list(jav_yerr) )
+                    for j in range(len(y)):
+                        self.assertAlmostEqual( y[j], jav_y[j], places=6 )
+
+
+
+        lag_bounds = [ [-1000, 1000], [-500, 500] ]
+        #Make sure lag bounds worked
+        for i in range(num_dicts):
+            self.assertGreaterEqual( np.min(self.res['javelin_res'][i]['tophat_params'][0]), lag_bounds[i][0] )
+            self.assertLessEqual( np.max(self.res['javelin_res'][i]['tophat_params'][0]), lag_bounds[i][1] )
+
+
+
+    #Make sure the layout of the files is correct
+    def test_file_layout(self):
+        main_directories = glob.glob('.tmp/*/')
+        subdirs = ['.tmp/continuum/', '.tmp/yelm/', '.tmp/zing/']
+
+        mc_length = 20*10
+        burn_length = 10*10
+
+        #Make sure each line has a subdirectory
+        self.assertIn( '.tmp/continuum/' , main_directories )
+        self.assertIn( '.tmp/yelm/' , main_directories )
+        self.assertIn( '.tmp/zing/' , main_directories )
+
+        #Make sure continuum has no files
+        self.assertEqual( len(glob.glob('.tmp/continuum/*')), 0 )
+
+        #Make sure each line has a "javelin" subdirectory
+        for fdir in subdirs[1:]:
+            subdir_dirs = glob.glob(fdir + '*')
+            self.assertIn( fdir + 'javelin' , subdir_dirs )
+
+        #Make sure each "javelin" subdirectory has a ccf and dist file
+        for i, fdir in zip([1,2], subdirs[1:]):
+            files = glob.glob(fdir + 'javelin/*')
+            for x in ['cont', 'rmap']:
+                self.assertIn( fdir + 'javelin/' + 'burn_' + x + '.txt', files )
+                self.assertIn( fdir + 'javelin/' + 'chain_' + x + '.txt', files )
+                self.assertIn( fdir + 'javelin/' + 'logp_' + x + '.txt', files )
+
+            self.assertIn( fdir + 'javelin/continuum_lc_fits.dat', files )
+            self.assertIn( fdir + 'javelin/' + self.line_names[i] + '_lc_fits.dat', files )
+
+            self.assertIn( fdir + 'javelin/cont_lcfile.dat', files )
+            self.assertIn( fdir + 'javelin/tot_lcfile.dat', files )            
+
+            self.assertIn( fdir + 'javelin/javelin_bestfit.pdf', files )
+            self.assertIn( fdir + 'javelin/javelin_corner.pdf', files )
+            self.assertIn( fdir + 'javelin/javelin_histogram.pdf', files )
+
+
+
+
+            #Make sure the lc files are correct
+            xcont, ycont, yerrcont = np.loadtxt( self.filenames[0], unpack=True, usecols=[0,1,2] )
+            xline, yline, yerrline = np.loadtxt( self.filenames[i], unpack=True, usecols=[0,1,2] )
+
+            cont_dat = javelin.zylc.get_data( fdir + 'javelin/cont_lcfile.dat' )
+            jav_x = cont_dat.jlist[0]
+            jav_y = cont_dat.mlist[0] + cont_dat.blist[0]
+            jav_yerr = cont_dat.elist[0]
+
+            self.assertListEqual( list(xcont), list(cont_dat.jlist[0]) )
+            self.assertListEqual( list(ycont), list(cont_dat.mlist[0] + cont_dat.blist[0]) )            
+            for j in range(len(ycont)):
+                self.assertAlmostEqual( ycont[j], jav_y[j], places=6 )
+
+            tot_dat = javelin.zylc.get_data( fdir + 'javelin/tot_lcfile.dat' )
+            jav_x = tot_dat.jlist[0]
+            jav_y = tot_dat.mlist[0] + tot_dat.blist[0]
+            jav_yerr = tot_dat.elist[0]
+
+            self.assertListEqual( list(xcont), list(tot_dat.jlist[0]) )
+            self.assertListEqual( list(yerrcont), list(tot_dat.elist[0]) )
+            for j in range(len(jav_x)):
+                self.assertAlmostEqual( ycont[j], jav_y[j], places=6 )
+
+
+
+            jav_x = tot_dat.jlist[1]
+            jav_y = tot_dat.mlist[1] + tot_dat.blist[1]
+            jav_yerr = tot_dat.elist[1]
+
+            self.assertListEqual( list(xline), list(tot_dat.jlist[1]) )
+            self.assertListEqual( list(yerrline), list(tot_dat.elist[1]) )
+            for j in range(len(jav_x)):
+                self.assertAlmostEqual( yline[j], jav_y[j], places=6 )
+
+
+
+            #Make sure chain and burn files have correct length
+            for x in ['cont', 'rmap']:
+                for prefix in ['burn', 'chain', 'logp']:
+                    chain = np.loadtxt( fdir + 'javelin/' + prefix + '_' + x + '.txt' )
+                    self.assertEqual( len(chain), burn_length if prefix == 'burn' else mc_length )
+
+
+        #Make sure the light curves get saved
+        self.assertIn( '.tmp/light_curves/', main_directories )
+
+        lc_files = glob.glob('.tmp/light_curves/*')
+        for name in self.line_names:
+            self.assertIn( '.tmp/light_curves/' + name + '.dat', lc_files )
+
+        
+
+
+    def test_res_file_match(self):
+
+        #Match dists        
+        for i in range(2):
+
+            file_sig, file_tau, t, w, s = np.loadtxt( '.tmp/' + self.line_names[i+1] + '/javelin/chain_rmap.txt', unpack=True )
+            file_sig = np.exp(file_sig)
+            file_tau = np.exp(file_tau)
+            file_tophat = [t, w, s]
+
+            self.assertListEqual( list(file_sig), list(self.res['javelin_res'][i]['sigma']) )
+            self.assertListEqual( list(file_tau), list(self.res['javelin_res'][i]['tau']) )
+            for j in range(3):
+                self.assertListEqual( list(file_tophat[j]), list(self.res['javelin_res'][i]['tophat_params'][j]) )
+
