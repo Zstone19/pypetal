005cf40d7dc5d570f7fbfcd531505200f8c5453a
Zstone19 <stone28@illinois.edu>
2023-02-01 14:07:09 -0600

    Still trying...

--- src/pypetal/plotting.py
+++ src/pypetal/plotting.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 
+import javelin.lcmodel
+
 from matplotlib.colors import ListedColormap
 import palettable
 
