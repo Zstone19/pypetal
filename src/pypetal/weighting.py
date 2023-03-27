6d0ea85e4867a3a42329b28f0c421705a02a03e8
Zstone19 <stone28@illinois.edu>
2023-03-27 03:18:16 -0500

    Forgot to import os in weighting

--- src/pypetal/weighting.py
+++ src/pypetal/weighting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from matplotlib import gridspec
 from mpl_toolkits.axes_grid1.inset_locator import (BboxConnector,
                                                    TransformedBbox, inset_axes)
