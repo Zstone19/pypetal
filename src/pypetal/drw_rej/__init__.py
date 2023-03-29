3c030fe7d7333fac8b88323123bcf46d18e434c8
Zstone19 <stone28@illinois.edu>
2023-03-29 02:46:20 -0500

    Still trying...

--- src/pypetal/drw_rej/__init__.py
+++ src/pypetal/drw_rej/__init__.py
@@ -1,5 +1,4 @@
 from .module import *
-from .plotting import *
 from .utils import *
 
-__all__ = ["pypetal.drw_rej.module", "pypetal.drw_rej.plotting", "pypetal.drw_rej.utils"]
+__all__ = ["pypetal.drw_rej.module", "pypetal.drw_rej.utils"]
