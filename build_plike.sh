b3b845adce1d2e33f4ed309ffbd89dbc830221e9
Zstone19 <stone28@illinois.edu>
2023-04-08 15:51:21 -0500

    Updated build_plike.sh

--- build_plike.sh
+++ build_plike.sh
@@ -2,9 +2,9 @@
 # Building PLIKE
 
 install_plike () {
-    mkdir plike_v4
+#    mkdir plike_v4
     cd plike_v4
-    wget http://www.weizmann.ac.il/home/tal/zdcf/plike_v4.0.f90
+#    wget http://www.weizmann.ac.il/home/tal/zdcf/plike_v4.0.f90
     gfortran plike_v4.0.f90 -o plike
     cd ..
 }
