012db8073ef8dca930ee43c16bc30a57a2b67ed6
Zstone19 <stone28@illinois.edu>
2023-04-09 23:51:12 -0500

    Added basic test (old test is now together test)

--- tests/test_pyroa/together_test.py
+++ tests/test_pyroa/together_test.py
@@ -13,7 +13,7 @@ class TestPyROA(unittest.TestCase):
 
     def setUp(self):
 
-        main_dir = 'pypetal/examples/dat/javelin_'
+        main_dir = 'examples/dat/javelin_'
         filenames = [main_dir + 'continuum.dat', main_dir + 'yelm.dat', main_dir + 'zing.dat']
 
         output_dir = '.tmp/'
@@ -25,7 +25,7 @@ class TestPyROA(unittest.TestCase):
             'add_var': False,
             'delay_dist': False,
             'subtract_mean': True,
-            'together': False
+            'together': True
         }
         lag_bounds = [ [-1000, 1000], [-500, 500] ]
 
@@ -48,18 +48,15 @@ class TestPyROA(unittest.TestCase):
         #################################################################################################
         #Make sure the lengths and keys of each of the resulting arrays are correct
 
+        self.assertIs( isinstance( self.res['pyroa_res'], MyFit ), True )
         mc_length = 100
         nburn = 50
-        nparams = 2 + 3 + 1
+        nparams = 2 + 3*2 + 1
 
-        self.assertEqual( len(self.res['pyroa_res']), len(self.line_names)-1 )
-        for i in range(len(self.line_names)-1):
-            self.assertTrue( isinstance( self.res['pyroa_res'][i], MyFit ) )
-
-            #Make sure lengths of arrays are correct
-            self.assertEqual( self.res['pyroa_res'][i].samples.shape[0], mc_length )
-            self.assertEqual( self.res['pyroa_res'][i].samples.shape[2], nparams )
-            self.assertEqual( len(self.res['pyroa_res'][i].models), 2 )
+        #Make sure lengths of arrays are correct
+        self.assertEqual( self.res['pyroa_res'].samples.shape[0], mc_length )
+        self.assertEqual( self.res['pyroa_res'].samples.shape[2], nparams )
+        self.assertEqual( len(self.res['pyroa_res'].models), 3 )
 
 
         #################################################################################################
@@ -78,37 +75,33 @@ class TestPyROA(unittest.TestCase):
         #Make sure continuum has no files
         self.assertEqual( len(glob.glob('.tmp/continuum/*')), 0 )
 
-        #Make sure each line has a "pyroa" subdirectory
+        #Make sure each line doesn't have a "pyroa" subdirectory
         for fdir in subdirs[1:]:
             subdir_dirs = glob.glob(fdir + '*')
-            self.assertIn( fdir + 'pyroa' , subdir_dirs )
+            self.assertNotIn( fdir + 'pyroa' , subdir_dirs )
 
-        #Make sure there is not a main pyroa directory
-        self.assertNotIn( '.tmp/pyroa/', main_directories )
+        #Make sure there is one main pyroa directory
+        self.assertIn( '.tmp/pyroa/', main_directories )
 
-
-        #Make sure each "pyroa" subdirectory has PyROA files
-        for i in range(len(self.line_names)-1):
-            files = glob.glob( subdirs[i+1] + 'pyroa/*')
-            self.assertIn( subdirs[i+1] + 'pyroa/samples.obj' , files )
-            self.assertIn( subdirs[i+1] + 'pyroa/samples_flat.obj' , files )
-            self.assertIn( subdirs[i+1] + 'pyroa/Lightcurve_models.obj' , files )
-            self.assertIn( subdirs[i+1] + 'pyroa/X_t.obj' , files )
+        #Make sure "pyroa" subdirectory has PyROA files
+        files = glob.glob('.tmp/pyroa/*')
+        self.assertIn( '.tmp/pyroa/samples.obj' , files )
+        self.assertIn( '.tmp/pyroa/samples_flat.obj' , files )
+        self.assertIn( '.tmp/pyroa/Lightcurve_models.obj' , files )
+        self.assertIn( '.tmp/pyroa/X_t.obj' , files )
 
         #Make sure "pyroa" subdirectory has PyROA plots
-        for i in range(len(self.line_names)-1):
-            files = glob.glob( subdirs[i+1] + 'pyroa/*')
-            self.assertIn( subdirs[i+1] + 'pyroa/corner_plot.pdf' , files )
-            self.assertIn( subdirs[i+1] + 'pyroa/fits_plot.pdf' , files )
-            self.assertIn( subdirs[i+1] + 'pyroa/histogram_plot.pdf' , files )
-            self.assertIn( subdirs[i+1] + 'pyroa/trace_plot.pdf' , files )
+        files = glob.glob('.tmp/pyroa/*')
+        self.assertIn( '.tmp/pyroa/corner_plot.pdf' , files )
+        self.assertIn( '.tmp/pyroa/fits_plot.pdf' , files )
+        self.assertIn( '.tmp/pyroa/histogram_plot.pdf' , files )
+        self.assertIn( '.tmp/pyroa/trace_plot.pdf' , files )
 
         #Make sure PyROA light curves get saved
         self.assertIn( '.tmp/pyroa_lcs/', main_directories )
-
         lc_files = glob.glob('.tmp/pyroa_lcs/*')
-        for i in range(len(self.line_names)):
-            self.assertIn( '.tmp/pyroa_lcs/pyroa_' + self.line_names[i] + '.dat', lc_files )
+        for name in self.line_names:
+            self.assertIn( '.tmp/pyroa_lcs/pyroa_' + name + '.dat', lc_files )
 
 
         #Make sure the light curves get saved
@@ -125,22 +118,21 @@ class TestPyROA(unittest.TestCase):
         #################################################################################################
 
         #get_samples_chunks
-        for i in range(len(self.line_names)-1):
-            samples = self.res['pyroa_res'][i].samples
-            nwalker = samples.shape[1]
+        samples = self.res['pyroa_res'].samples
+        nwalker = samples.shape[1]
 
-            samples_chunks = get_samples_chunks(samples, nburn)
-            nchain_tot = (mc_length-nburn)*nwalker
+        samples_chunks = get_samples_chunks(samples, nburn)
+        nchain_tot = (mc_length-nburn)*nwalker
 
-            self.assertEqual( len(samples_chunks), 3 )
+        self.assertEqual( len(samples_chunks), 4 )
 
-            for i in range(2):
-                self.assertEqual( len(samples_chunks[i]), 3 )
-                for j in range(3):
-                    self.assertEqual( len(samples_chunks[i][j]), nchain_tot )
+        for i in range(3):
+            self.assertEqual( len(samples_chunks[i]), 3 )
+            for j in range(3):
+                self.assertEqual( len(samples_chunks[i][j]), nchain_tot )
 
-            self.assertEqual( len(samples_chunks[-1]), 1 )
-            self.assertEqual( len(samples_chunks[-1][0]), nchain_tot )
+        self.assertEqual( len(samples_chunks[-1]), 1 )
+        self.assertEqual( len(samples_chunks[-1][0]), nchain_tot )
 
 
         #################################################################################################
@@ -149,7 +141,6 @@ class TestPyROA(unittest.TestCase):
 
         #Make sure the pyroa_lcs are equal to the original ones (with the mean subtracted)
         for i in range(len(self.line_names)):
-
             x1, y1, yerr1 = np.loadtxt( self.filenames[i], unpack=True, usecols=[0,1,2] )
             x2, y2, yerr2 = np.loadtxt( '.tmp/pyroa_lcs/pyroa_' + self.line_names[i] + '.dat', unpack=True, usecols=[0,1,2] )
 
